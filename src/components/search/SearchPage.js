5307a886d99d4c63b5c572a5b47d4aee0a6ea317
edubrak <dubeyraksha05@gmail.com>
2022-10-14 19:32:42 +0530

    zomato-clone

---
+++ src/components/search/SearchPage.js
@@ -0,0 +1,13 @@
+import Header from "../Header";
+import SearchPageResult from "./SearchPageResult";
+
+function SearchPage() {
+  return (
+    <>
+      <Header />
+      <SearchPageResult />
+    </>
+  );
+}
+
+export default SearchPage;
